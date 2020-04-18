157a52550ababce31da120963133c2746ec1623f
nnpgah <nnpgah2305@outlook.com>
2020-04-18 12:54:29 +0700

    Add comment

--- Training/Controllers/HomeController.cs
+++ Training/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Training.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";  
+            ViewBag.Message = "Your application description page.";  //123
 
             return View();
         }
